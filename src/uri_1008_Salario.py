2cd040e939af9ac6c081d1524e6960143b871406
Alessandro Silva <alessandro-psilva@outlook.com>
2020-11-21 11:40:59 -0300

    "https://www.urionlinejudge.com.br/judge/pt/problems/view/1008"

---
+++ src/uri_1008_Salario.py
@@ -0,0 +1,8 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1008
+nfunc = int(input())
+qhextra = int(input())
+vhextra = float(input())
+vtot_hextra = qhextra * vhextra
+
+print("NUMBER = {}".format(nfunc))
+print("SALARY = U$ {:.2f}".format(vtot_hextra))
